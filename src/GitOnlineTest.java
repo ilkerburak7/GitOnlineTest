acf44a7b233b2cf188c8ca2d87cdc3ef6d21b828
ilkerburak7 <ilkerburakgulec@gmail.com>
2021-03-16 19:48:17 -0500

    Project has been created By Ilker

---
+++ src/GitOnlineTest.java
@@ -0,0 +1,9 @@
+public class GitOnlineTest {
+    public static void main(String[] args) {
+
+        System.out.println("OnlineGang");
+        System.out.println("Sampiyon Besiktas");
+
+
+    }
+}
