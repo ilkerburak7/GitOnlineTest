24e7e21c65fe36ed7903e0131a6dac6240f000a2
mohsenamir <Amir1411>
2021-03-16 20:16:46 -0500

    updated by Amir

--- src/GitOnlineTest.java
+++ src/GitOnlineTest.java
@@ -1,9 +1,11 @@
 public class GitOnlineTest {
+
     public static void main(String[] args) {
 
         System.out.println("OnlineGang");
         System.out.println("Sampiyon Besiktas");
-
+        System.out.println("Amir");
+        System.out.println("Techtorial");
 
     }
 }
