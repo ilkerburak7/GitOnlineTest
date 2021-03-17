facf2fa030cd3ea52fccff84e09a8822a6ede1f0
dina148 <gulaymamytova21@gmail.com>
2021-03-16 21:06:19 -0500

    Test 1

--- src/GitOnlineTest.java
+++ src/GitOnlineTest.java
@@ -1,9 +1,43 @@
 public class GitOnlineTest {
+
     public static void main(String[] args) {
 
-        System.out.println("OnlineGang");
+        System.out.println("OnlineGangTeam");
+        System.out.println("asdasd");
+        System.out.println("asdasdasdasd");
+        System.out.println("asdasdasdasfasdasfasd");
+        System.out.println("ILKEEEEEEER");
+        System.out.println("Asasdasdas");
+        System.out.println("asdasdasdqwewqe");
+        System.out.println("asdqweqwewqe");
+        System.out.println("Sampiyon Besiktas");
+        System.out.println("ILKEEEEEEER");
+        System.out.println("Last change");
+        System.out.println("Asasdasdas");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasdqwewqe");
+        System.out.println("asdqweqwewqe");
         System.out.println("Sampiyon Besiktas");
 
+
+        System.out.println("Basak Ciftci");
+
+        System.out.println("Amir");
+
+        System.out.println("asdasdasdasd");
+        System.out.println("Asdasdasd");
+  
+         System.out.println("Techtorial");
+         System.out.println("Kunduz");
+         System.out.println("Kunduz");
+         System.out.println("SDET");
+        System.out.println("Gulya148");
+         
+        
+
+       
+ 
+        
 
     }
 }
